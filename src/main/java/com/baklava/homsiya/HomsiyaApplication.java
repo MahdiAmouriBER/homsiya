2390abffe5dc6b11621f05bb6b374f476cd79520
Diriyah <mahdiamouri731@gmail.com>
2024-02-27 22:08:08 +0100

    Initial commit

---
+++ src/main/java/com/baklava/homsiya/HomsiyaApplication.java
@@ -0,0 +1,13 @@
+package com.baklava.homsiya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomsiyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomsiyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baklava/homsiya/HomsiyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baklava.homsiya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomsiyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
